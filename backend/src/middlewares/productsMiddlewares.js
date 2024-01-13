279053d08c17e72623eb2a37e5da7e8d658f4907
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2024-01-13 16:35:36 -0300

    feat: req 04

--- backend/src/middlewares/productsMiddlewares.js
+++ backend/src/middlewares/productsMiddlewares.js
@@ -1,8 +1,14 @@
 function checkProductName(req, res, next) {
   const { name } = req.body;
+  
   if (!name) {
-    return res.status(400).json({ error: 'O nome do produto é obrigatório.' });
+    return res.status(400).json({ message: '"name" is required' });
   }
+
+  if (name.length < 5) {
+    return res.status(422).json({ message: '"name" length must be at least 5 characters long' });
+  }
+
   next(); 
 }
 
