dcdf14f466582846a748dc6d2cf274d89cb45c6e
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2024-01-13 14:30:45 -0300

    fix erro conection

--- backend/src/database/conection.js
+++ backend/src/database/conection.js
@@ -1,8 +1,9 @@
 const dbConfig = {
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'StoreManager',
+  host: process.env.MYSQL_HOSTNAME || 'localhost',
+  port: process.env.MYSQL_PORT || 3306,
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'password',
+  database: process.env.MYSQL_DATABASE || 'StoreManager',
 };
 
 module.exports = dbConfig;
